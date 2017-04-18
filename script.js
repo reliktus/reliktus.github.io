4d97208ebbbd4b87f1a8f0a9197257c84aa91dc4
Rafał <reliktus@gmail.com>
2017-04-18 12:30:30 +0200

    test intellij

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
 $(document).ready(function() {
     $("img").addClass("img-responsive");
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('#back-to-top').fadeIn();
-        } else {
-            $('#back-to-top').fadeOut();
-        }
-    });
-    // scroll body to 0px on click
-    $('#back-to-top').click(function () {
-        $('#back-to-top').tooltip('hide');
-        $('body,html').animate({
-            scrollTop: 0
-        }, 800);
-        return false;
-    });
-
-    $('#back-to-top').tooltip('show');
-
-});
-/**
- * Created by rlkt on 2017-04-13.
- */
+})
