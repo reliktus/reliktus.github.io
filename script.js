26d3b0af0e560dd012bc8ad6fd9f36a1d20e29e3
Rafał <reliktus@gmail.com>
2017-04-19 12:46:23 +0200

    test intellij

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function imgStyler() {
-
-        $("img").addClass("img-responsive img-rounded")
+        $(".projectImg").addClass("img-responsive img-rounded ")
 }
 var mail_user = 'jaworski.inf';
-var domain = 'gmail.com';
-var mScript1 = "mailto:";
-var mScript2 = "@";
+var domain ='gmail.com';
+var mScript1 ="mailto:";
+var mScript2 ="@";
