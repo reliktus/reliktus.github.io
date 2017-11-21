ef32b2cfeb480fa63298d132f8596acb112644e0
relikt <reliktus@gmail.com>
2017-11-21 21:34:06 +0100

    Refactor + add project + ad myFaceImg

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function smoothScroll() {
 }
 function skillsList() {
     let about = '<p><h5>Software Engineer with a passion for understanding things at a fundamental level.<br>' +
-        'Fascinated about Front-end development and Java Script frameworks.</h5></p><br>';
+        'Fascinated about Front-end development and JavaScript frameworks.</h5></p><br>';
     let skillsArr = [
         "HTML5 + CSS 3", "JavaScript ES6", "jQuery", 'TypeScript', 'Angular 4 + CLI', "Sass / SCSS", "Webpack", "RWD: Bootstrap", "NPM", "PHP", "GIT", 'XAMPP', "Batch", "Core JAVA", "IDE: Intellij + Eclipse", "Active Directory", "VBA"
 
